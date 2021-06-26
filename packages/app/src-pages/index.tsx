c2d369e824071591400cbf0bb5a544afc7566100
Michael Arnaldi <arnaldimichael@gmail.com>
2021-06-26 20:03:29 +0100

    enable incremental

--- packages/app/src-pages/index.tsx
+++ packages/app/src-pages/index.tsx
@@ -2,7 +2,7 @@ import * as O from "@effect-ts/core/Option";
 import * as React from "react";
 
 function Home() {
-  return <div>{O.some("OK").value}</div>;
+  return <div>{O.some("OK OK").value}</div>;
 }
 
 export default Home;
