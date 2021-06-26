2743494fbb7e5f0f706cbfc978fbb76218dbee75
Michael Arnaldi <arnaldimichael@gmail.com>
2021-06-26 20:19:51 +0100

    build

---
+++ packages/app/next.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+module.exports = {
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
--- packages/app/src-pages/index.tsx
+++ packages/app/src-pages/index.tsx
@@ -1,3 +1,5 @@
+// tracing: off
+
 import * as O from "@effect-ts/core/Option";
 import * as React from "react";
 
