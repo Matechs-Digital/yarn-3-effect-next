388268e65e75906be4fd41d8433fc17017243d7e
Michael Arnaldi <arnaldimichael@gmail.com>
2021-06-27 15:29:34 +0100

    remove comment

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-undef */
 module.exports = {
   eslint: {
-    // Warning: Dangerously allow production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   target: "serverless",
