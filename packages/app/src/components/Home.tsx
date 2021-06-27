ab33289c19dcae68d52dd5dc00c7a4d152d4c96c
Michael Arnaldi <arnaldimichael@gmail.com>
2021-06-27 14:51:51 +0100

    update

--- packages/app/src/components/Home.tsx
+++ packages/app/src/components/Home.tsx
@@ -1,7 +1,17 @@
+import * as T from "@effect-ts/core/Effect";
 import * as O from "@effect-ts/core/Option";
 import { underline } from "app/common/Styles";
 import * as React from "react";
 
 export function Home() {
-  return <div style={underline}>{O.some("It works").value}</div>;
+  const [counter, updateCounter] = React.useState(0);
+
+  const positive = O.fromPredicate((n: number) => n > 0);
+  const message = positive(counter).chain((n) => O.some(`It works: ${n}`));
+
+  React.useEffect(() => {
+    T.succeedWith(() => updateCounter((n) => n + 1)).runPromise();
+  }, []);
+
+  return <div style={underline}>{message.value || "Loading..."}</div>;
 }
