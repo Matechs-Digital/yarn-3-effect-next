f4bf176ec45657aa45733c488fadd7ea72cdfbe6
Michael Arnaldi <arnaldimichael@gmail.com>
2021-06-27 14:59:16 +0100

    update

--- packages/app/src/components/Home.tsx
+++ packages/app/src/components/Home.tsx
@@ -6,11 +6,13 @@ import * as React from "react";
 export function Home() {
   const [counter, updateCounter] = React.useState(0);
 
-  const positive = O.fromPredicate((n: number) => n > 0);
-  const message = positive(counter).chain((n) => O.some(`It works: ${n}`));
+  const positive = counter > 0 ? O.some(counter) : O.none;
+  const message = positive.chain((n) => O.some(`It works: ${n}`));
 
   React.useEffect(() => {
-    T.succeedWith(() => updateCounter((n) => n + 1)).runPromise();
+    T.sleep(1_000)
+      .zipRight(T.succeedWith(() => updateCounter((n) => n + 1)))
+      .runPromise();
   }, []);
 
   return <div style={underline}>{message.value || "Loading..."}</div>;
